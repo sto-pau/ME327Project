492185866c2753afaec65fa5797d07533a66740f
Danny Raymond Pugh <drpugh@stanford.edu>
2022-05-20 14:50:49 -0700

    Add dummy program for secondary board and base arduino code

---
+++ dummy_code/dummy_code.ino
@@ -0,0 +1,13 @@
+// Pin declares
+int sensorPosPin = A2; // input pin for MR sensor
+
+void setup() {
+  // put your setup code here, to run once:
+  // Input pins
+  pinMode(sensorPosPin, INPUT); // set MR sensor pin to be an input
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
