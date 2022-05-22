7d2d30de0bb1d9ad4bd4d04bc4f6037b2fd2edc5
Danny Raymond Pugh <drpugh@stanford.edu>
2022-05-22 12:22:22 -0700

    Add RIGHT and LEFT motor code (not tested) Added reformat

--- Pantagraph_kinematics_test_code/Pantagraph_kinematics_test_code.ino
+++ Pantagraph_kinematics_test_code/Pantagraph_kinematics_test_code.ino
@@ -5,9 +5,9 @@
 
 // Includes
 #include <math.h>
-#include <AS5048A.h> 
-// the sensor CSn pin is connected to pin 10 
-AS5048A angleSensor(10); 
+#include <AS5048A.h>
+// the sensor CSn pin is connected to pin 10
+AS5048A angleSensor(10);
 
 // generate constants
 double a1 = 129; //mm
@@ -17,10 +17,10 @@ double a4 = 129; //mm
 double a5 = 82; //mm
 
 // Pin declares
-int pwmPinR = 5; // PWM output pin for motor 
-int dirPinR = 8; // direction output pin for motor 
-int pwmPinL = 6; // PWM output pin for motor 
-int dirPinL = 7; // direction output pin for motor 
+int pwmPinR = 5; // PWM output pin for motor
+int dirPinR = 8; // direction output pin for motor
+int pwmPinL = 6; // PWM output pin for motor
+int dirPinL = 7; // direction output pin for motor
 int sensorPosPin = A4; // input pin for MR sensor
 int fsrPin = A3; // input pin for FSR sensor
 
@@ -56,8 +56,8 @@ boolean flippedHE = false;
 double OFFSETHE = 16000;
 double OFFSET_NEGHE = 15;
 
-// Kinematics 
-//variables 
+// Kinematics
+//variables
 double xh = 0;
 double yh = 0;
 double dxh = 0;
@@ -79,58 +79,60 @@ double dtheta5_prev;
 double dtheta5_prev2;
 
 // Force output variables
-double force = 0;           // force at the handle
-double forcey = 0;
+double forceX = 0;           // force at the handle
+double forceY = 0;
 
-double Tp = 0;              // torque of the motor pulley
+double TpR = 0;              // torque of the motor pulley
 double TpL = 0;              // torque of the motor pulley
 
-double duty = 0;            // duty cylce (between 0 and 255)
-unsigned int output = 0;    // output command to the motor
+double dutyR = 0;            // duty cylce (between 0 and 255)
+unsigned int outputR = 0;    // output command to the motor
+double dutyL = 0;            // duty cylce (between 0 and 255)
+unsigned int outputL = 0;    // output command to the motor
 
 
 // --------------------------------------------------------------
 // Setup function -- NO NEED TO EDIT
 // --------------------------------------------------------------
-void setup() 
+void setup()
 {
 
-  angleSensor.init(); 
-  
+  angleSensor.init();
+
   // Set up serial communication
   Serial.begin(115200);
-  
-  // Set PWM frequency 
-  setPwmFrequency(pwmPinR,1); 
-  
+
+  // Set PWM frequency
+  setPwmFrequency(pwmPinR, 1);
+
   // Input pins
   pinMode(sensorPosPin, INPUT); // set MR sensor pin to be an input
   pinMode(fsrPin, INPUT);       // set FSR sensor pin to be an input
 
   // Output pins
-  pinMode(pwmPinR, OUTPUT);  // PWM pin for motor 
-  pinMode(dirPinR, OUTPUT);  // dir pin for motor 
+  pinMode(pwmPinR, OUTPUT);  // PWM pin for motor
+  pinMode(dirPinR, OUTPUT);  // dir pin for motor
 
-    // Output pins
-  pinMode(pwmPinL, OUTPUT);  // PWM pin for motor 
-  pinMode(dirPinL, OUTPUT);  // dir pin for motor 
-  
-  // Initialize motor 
+  // Output pins
+  pinMode(pwmPinL, OUTPUT);  // PWM pin for motor
+  pinMode(dirPinL, OUTPUT);  // dir pin for motor
+
+  // Initialize motor
   analogWrite(pwmPinR, 0);     // set to not be spinning (0/255)
   digitalWrite(dirPinR, LOW);  // set direction
 
-  // Initialize motor 
+  // Initialize motor
   analogWrite(pwmPinL, 0);     // set to not be spinning (0/255)
   digitalWrite(dirPinL, LOW);  // set direction
-  
+
   // Initialize position valiables
   lastLastRawPos = analogRead(sensorPosPin);
   lastRawPos = analogRead(sensorPosPin);
   flipNumber = 0;
 
-  // Initialize position variables 
-  angleSensor.getRawRotation(); // take reading in case first reading is spurious 
-  lastLastRawPosHE = angleSensor.getRawRotation(); //position from Hall Effect sensor 
+  // Initialize position variables
+  angleSensor.getRawRotation(); // take reading in case first reading is spurious
+  lastLastRawPosHE = angleSensor.getRawRotation(); //position from Hall Effect sensor
   lastRawPosHE = angleSensor.getRawRotation(); //position from Hall Effect sensor
 }
 
@@ -140,9 +142,9 @@ void setup()
 // --------------------------------------------------------------
 void loop()
 {
-  
+
   //*************************************************************
-  //*** Section 1. Compute position in counts (do not change) ***  
+  //*** Section 1. Compute position in counts (do not change) ***
   //*************************************************************
 
   // Get voltage output by MR sensor
@@ -153,15 +155,15 @@ void loop()
   lastRawDiff = rawPos - lastLastRawPos;  //difference btwn current raw position and last last raw position
   rawOffset = abs(rawDiff);
   lastRawOffset = abs(lastRawDiff);
-  
+
   // Update position record-keeping vairables
   lastLastRawPos = lastRawPos;
   lastRawPos = rawPos;
-  
+
   // Keep track of flips over 180 degrees
-  if((lastRawOffset > flipThresh) && (!flipped)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
-    if(lastRawDiff > 0) {        // check to see which direction the drive wheel was turning
-      flipNumber--;              // cw rotation 
+  if ((lastRawOffset > flipThresh) && (!flipped)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
+    if (lastRawDiff > 0) {       // check to see which direction the drive wheel was turning
+      flipNumber--;              // cw rotation
     } else {                     // if(rawDiff < 0)
       flipNumber++;              // ccw rotation
     }
@@ -169,30 +171,30 @@ void loop()
   } else {                        // anytime no flip has occurred
     flipped = false;
   }
-   updatedPos = rawPos + flipNumber*OFFSET; // need to update pos based on what most recent offset is 
+  updatedPos = rawPos + flipNumber * OFFSET; // need to update pos based on what most recent offset is
 
 
   //*************************************************************
-  //*** Section 1. Compute position in counts (do not change) ***  
+  //*** Section 1. Compute position in counts (do not change) ***
   //*************************************************************
-  
-  // Get angle output by Hall Effect sensor 
-  rawPosHE = int(angleSensor.getRawRotation()); //position from Hall Effect sensor 
 
-    // Calculate differences between subsequent MR sensor readings
+  // Get angle output by Hall Effect sensor
+  rawPosHE = int(angleSensor.getRawRotation()); //position from Hall Effect sensor
+
+  // Calculate differences between subsequent MR sensor readings
   rawDiffHE = rawPosHE - lastRawPosHE;          //difference btwn current raw position and last raw position
   lastRawDiffHE = rawPosHE - lastLastRawPosHE;  //difference btwn current raw position and last last raw position
   rawOffsetHE = abs(rawDiffHE);
   lastRawOffsetHE = abs(lastRawDiffHE);
-  
+
   // Update position record-keeping vairables
   lastLastRawPosHE = lastRawPosHE;
   lastRawPosHE = rawPosHE;
-  
+
   // Keep track of flips over 180 degrees
-  if((lastRawOffsetHE > flipThreshHE) && (!flippedHE)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
-    if(lastRawDiffHE > 0) {        // check to see which direction the drive wheel was turning
-      flipNumberHE--;              // cw rotation 
+  if ((lastRawOffsetHE > flipThreshHE) && (!flippedHE)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
+    if (lastRawDiffHE > 0) {       // check to see which direction the drive wheel was turning
+      flipNumberHE--;              // cw rotation
     } else {                     // if(rawDiff < 0)
       flipNumberHE++;              // ccw rotation
     }
@@ -200,25 +202,25 @@ void loop()
   } else {                        // anytime no flip has occurred
     flippedHE = false;
   }
-   updatedPosHE = rawPosHE + flipNumberHE*OFFSETHE; // need to update pos based on what most recent offset is 
+  updatedPosHE = rawPosHE + flipNumberHE * OFFSETHE; // need to update pos based on what most recent offset is
 
- 
+
   //*************************************************************
   //*** Section 2. Compute position in meters *******************
   //*************************************************************
 
   // ADD YOUR CODE HERE
   // Define kinematic parameters you may need
-     //double rh = ?;   //[m]
+  //double rh = ?;   //[m]
   // Step B.1: print updatedPos via serial monitor
   //Serial.println((float)updatedPosHE,5);
   //Serial.println((float)updatedPos,5);
   // Step B.6: double ts = ?; // Compute the angle of the sector pulley (ts) in degrees based on updatedPos
-  double theta5 = (PI/180)*(updatedPos*-0.0129+155.5);
-  double theta1 = (PI/180)*(updatedPosHE*0.001470+38.09);
-//  Serial.print((float)theta5,5);
-//  Serial.print("\t");
-//  Serial.print((float)theta1,5);
+  double theta5 = (PI / 180) * (updatedPos * -0.0129 + 155.5);
+  double theta1 = (PI / 180) * (updatedPosHE * 0.001470 + 38.09);
+  //  Serial.print((float)theta5,5);
+  //  Serial.print("\t");
+  //  Serial.print((float)theta1,5);
   // Step B.7: xh = ?;       // Compute the position of the handle (in meters) based on ts (in radians)
   // Step B.8: print xh via serial monitor
 
@@ -238,30 +240,30 @@ void loop()
   theta5_prev2 = theta5_prev;
   theta5_prev = theta5;
 
-//  Serial.print("\t");
-//  Serial.print((float)dtheta5,5);
-//  Serial.print("\t");
-//  Serial.println((float)dtheta1,5);
-  
+  //  Serial.print("\t");
+  //  Serial.print((float)dtheta5,5);
+  //  Serial.print("\t");
+  //  Serial.println((float)dtheta1,5);
+
   //calculate positions 2 and 4
-  double P2x = a1*cos(theta1);
-  double P2y = a1*sin(theta1);
-  double P4x = a4*cos(theta5) - a5;
-  double P4y = a4*sin(theta5);
+  double P2x = a1 * cos(theta1);
+  double P2y = a1 * sin(theta1);
+  double P4x = a4 * cos(theta5) - a5;
+  double P4y = a4 * sin(theta5);
 
 
-  
+
   // calculate norms
-  double P42_norm = sqrt((P2x-P4x)*(P2x-P4x)+(P2y-P4y)*(P2y-P4y));
-  double P2h_norm = (a2*a2 - a3*a3 + P42_norm*P42_norm)/(2*P42_norm);
-  double P3h_norm = sqrt(a2*a2 - P2h_norm*P2h_norm);
+  double P42_norm = sqrt((P2x - P4x) * (P2x - P4x) + (P2y - P4y) * (P2y - P4y));
+  double P2h_norm = (a2 * a2 - a3 * a3 + P42_norm * P42_norm) / (2 * P42_norm);
+  double P3h_norm = sqrt(a2 * a2 - P2h_norm * P2h_norm);
 
   // calculate position h
-  double Phx = P2x + (P2h_norm/P42_norm)*(P4x-P2x);
-  double Phy = P2y + (P2h_norm/P42_norm)*(P4y-P2y);
+  double Phx = P2x + (P2h_norm / P42_norm) * (P4x - P2x);
+  double Phy = P2y + (P2h_norm / P42_norm) * (P4y - P2y);
   // calculate position 3
-  double P3x = Phx + (P3h_norm/P42_norm)*(P4y-P2y);
-  double P3y = Phy - (P3h_norm/P42_norm)*(P4x-P2x);
+  double P3x = Phx + (P3h_norm / P42_norm) * (P4y - P2y);
+  double P3y = Phy - (P3h_norm / P42_norm) * (P4x - P2x);
 
   xh = P3x;
   yh = P3y;
@@ -272,69 +274,96 @@ void loop()
   double h = P3h_norm;
   // calculate partial derivatives
   // d1 partial derivatives
-  double d1x2 = -a1*sin(theta1);
-  double d1y2 = a1*cos(theta1);
+  double d1x2 = -a1 * sin(theta1);
+  double d1y2 = a1 * cos(theta1);
   double d1y4 = 0;
   double d1x4 = 0;
-  double d1d = ( (P4x-P2x)*(d1x4-d1x2) + (P4y-P2y)*(d1y4-d1y2 ))/d;
-  double d1b = d1d - ( d1d*(a2*a2 - a3*a3 + d*d))/(2*d*d);
-  double d1h = -b*d1b/h;
-  double d1yh = d1y2 + ( (d1b*d - d1d*b)/(d*d) )*(P4y-P2y)+(b/d)*(d1y4-d1y2);
-  double d1xh = d1x2+( (d1b*d-d1b*d)/(d*d) )*(P4x-P2x)+(b/d)*(d1x4-d1x2);
-  double d1x3 = d1xh + (h/d)*(d1y4 - d1y2) + (d1h*d - d1d*h)/(d*d)*(P4y - P2y);
-  double d1y3 = d1yh + (h/d)*(d1x4 - d1x2) + (d1h*d - d1d*h)/(d*d)*(P4x - P2x);
+  double d1d = ( (P4x - P2x) * (d1x4 - d1x2) + (P4y - P2y) * (d1y4 - d1y2 )) / d;
+  double d1b = d1d - ( d1d * (a2 * a2 - a3 * a3 + d * d)) / (2 * d * d);
+  double d1h = -b * d1b / h;
+  double d1yh = d1y2 + ( (d1b * d - d1d * b) / (d * d) ) * (P4y - P2y) + (b / d) * (d1y4 - d1y2);
+  double d1xh = d1x2 + ( (d1b * d - d1b * d) / (d * d) ) * (P4x - P2x) + (b / d) * (d1x4 - d1x2);
+  double d1x3 = d1xh + (h / d) * (d1y4 - d1y2) + (d1h * d - d1d * h) / (d * d) * (P4y - P2y);
+  double d1y3 = d1yh + (h / d) * (d1x4 - d1x2) + (d1h * d - d1d * h) / (d * d) * (P4x - P2x);
   // d5 partial derivatives
-  double d5x4 = -a4*sin(theta5);
-  double d5y4 = a4*cos(theta5);
+  double d5x4 = -a4 * sin(theta5);
+  double d5y4 = a4 * cos(theta5);
   double d5y2 = 0;
   double d5x2 = 0;
-  double d5d = ( (P4x-P2x)*(d5x4-d5x2) + (P4y-P2y)*(d5y4-d5y2) )/d;
-  double d5b = d5d - ( d5d*(a2*a2 - a3*a3 + d*d))/(2*d*d);
-  double d5h = -b*d1b/h;
-  double d5yh = d5y2 + ( (d5b*d - d5d*b)/(d*d) )*(P4y-P2y)+(b/d)*(d5y4-d5y2);
-  double d5xh = d5x2+( (d5b*d-d5b*d)/(d*d) )*(P4x-P2x)+(b/d)*(d5x4-d5x2);;
-  double d5x3 = d5xh + (h/d)*(d5y4 - d5y2) + (d5h*d - d5d*h)/(d*d)*(P4y - P2y);
-  double d5y3 = d5yh + (h/d)*(d5x4 - d5x2) + (d5h*d - d5d*h)/(d*d)*(P4x - P2x);
+  double d5d = ( (P4x - P2x) * (d5x4 - d5x2) + (P4y - P2y) * (d5y4 - d5y2) ) / d;
+  double d5b = d5d - ( d5d * (a2 * a2 - a3 * a3 + d * d)) / (2 * d * d);
+  double d5h = -b * d1b / h;
+  double d5yh = d5y2 + ( (d5b * d - d5d * b) / (d * d) ) * (P4y - P2y) + (b / d) * (d5y4 - d5y2);
+  double d5xh = d5x2 + ( (d5b * d - d5b * d) / (d * d) ) * (P4x - P2x) + (b / d) * (d5x4 - d5x2);;
+  double d5x3 = d5xh + (h / d) * (d5y4 - d5y2) + (d5h * d - d5d * h) / (d * d) * (P4y - P2y);
+  double d5y3 = d5yh + (h / d) * (d5x4 - d5x2) + (d5h * d - d5d * h) / (d * d) * (P4x - P2x);
   //calculate velocity from omega (dTheta{i})
-  double dxh = d1x3*dtheta1 + d5x3*dtheta5;
-  double dyh = d1y3*dtheta1 + d5x3*dtheta5; 
-  //calculate torque from force 
-  Serial.print((float)dxh,3);
+  double dxh = d1x3 * dtheta1 + d5x3 * dtheta5;
+  double dyh = d1y3 * dtheta1 + d5x3 * dtheta5;
+  //calculate torque from force
+  Serial.print((float)dxh, 3);
   Serial.print(" : ");
-  Serial.println((float)dyh,3);
+  Serial.println((float)dyh, 3);
 
   //*************************************************************
-  //*** Section 3. Assign a motor output force in Newtons *******  
+  //*** Section 3. Assign a motor output force in Newtons *******
   //*************************************************************
- 
+
   // ADD YOUR CODE HERE
   // Define kinematic parameters you may need
-  
+  //VIRTUAL OBJECT HERE
+  // calculate forceX
+  // calculate forceY
+  // FORCE SIMULATION HERE
+  TR = d1x3*forceX + d1y3*forceY;
+  TL = d1x5*forceX + d1y5*forceY;
   // Step C.1: force = ?; // You can  generate a force by assigning this to a constant number (in Newtons) or use a haptic rendering / virtual environment
   // Step C.2: Tp = ?;    // Compute the require motor pulley torque (Tp) to generate that force using kinematics
- 
+
   //*************************************************************
   //*** Section 4. Force output (do not change) *****************
   //*************************************************************
-  
-  // Determine correct direction for motor torque
-  if(force > 0) { 
+
+  //COMMENT THIS TO CHECK TORQUE VALUE AND SIGN
+  // Determine correct direction for RIGHT motor torque
+  if (TR > 0) {
     digitalWrite(dirPinR, HIGH);
   } else {
     digitalWrite(dirPinR, LOW);
   }
 
   // Compute the duty cycle required to generate Tp (torque at the motor pulley)
-  duty = sqrt(abs(Tp)/0.0183);
+  dutyR = sqrt(abs(TR) / 0.0183);
 
   // Make sure the duty cycle is between 0 and 100%
-  if (duty > 1) {            
-    duty = 1;
-  } else if (duty < 0) { 
-    duty = 0;
-  }  
-  output = (int)(duty* 255);   // convert duty cycle to output signal
-  analogWrite(pwmPinR,output);  // output the signal
+  if (dutyR > 1) {
+    dutyR = 1;
+  } else if (dutyR < 0) {
+    dutyR = 0;
+  }
+  outputR = (int)(dutyR * 255);  // convert duty cycle to output signal
+  analogWrite(pwmPinR, outputR); // output the signal
+
+
+  // Determine correct direction for LEFT  motor torque
+  if (TL > 0) {
+    digitalWrite(dirPinL, HIGH);
+  } else {
+    digitalWrite(dirPinL, LOW);
+  }
+
+  // Compute the duty cycle required to generate Tp (torque at the motor pulley)
+  dutyL = sqrt(abs(TL) / 0.0183);
+
+  // Make sure the duty cycle is between 0 and 100%
+  if (dutyL > 1) {
+    dutyL = 1;
+  } else if (dutyL < 0) {
+    dutyL = 0;
+  }
+  outputL = (int)(dutyL * 255);  // convert duty cycle to output signal
+  analogWrite(pwmPinL, outputL); // output the signal
+  //STOP COMMENT OUT HERE
 }
 
 // --------------------------------------------------------------
@@ -342,8 +371,8 @@ void loop()
 // --------------------------------------------------------------
 void setPwmFrequency(int pin, int divisor) {
   byte mode;
-  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
-    switch(divisor) {
+  if (pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch (divisor) {
       case 1: mode = 0x01; break;
       case 8: mode = 0x02; break;
       case 64: mode = 0x03; break;
@@ -351,13 +380,13 @@ void setPwmFrequency(int pin, int divisor) {
       case 1024: mode = 0x05; break;
       default: return;
     }
-    if(pin == 5 || pin == 6) {
+    if (pin == 5 || pin == 6) {
       TCCR0B = TCCR0B & 0b11111000 | mode;
     } else {
       TCCR1B = TCCR1B & 0b11111000 | mode;
     }
-  } else if(pin == 3 || pin == 11) {
-    switch(divisor) {
+  } else if (pin == 3 || pin == 11) {
+    switch (divisor) {
       case 1: mode = 0x01; break;
       case 8: mode = 0x02; break;
       case 32: mode = 0x03; break;
