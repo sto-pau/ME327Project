0a83fefaf1567267774c5fb1dfaf837435ea2484
stoccop <stoccop@stanford.edu>
2022-05-23 23:52:35 -0700

    updated with Jacobian to calculate velocity and force, test with +/- forceX & forceY

--- Arduino_combined/Arduino_combined.ino
+++ Arduino_combined/Arduino_combined.ino
@@ -6,19 +6,21 @@
 //#define ENABLE_MASS_SPRING_DAMP 
 bool ENABLE_MASS_SPRING_DAMP = false;
 
-//#define DEBUGGING 
-#define TESTING 
+#define DEBUGGING 
+//#define TESTING 
+
+//constants here
+float unitsDivisor = 1000.0; 
 
 #define ARDBUFFER 16 //for serial line printing
 
 // Includes
 #include <math.h>
-#include <AS5048A.h> 
-// the sensor CSn pin is connected to pin 10 
-AS5048A angleSensor(10); 
+#include <AS5048A.h>
+// the sensor CSn pin is connected to pin 10
+AS5048A angleSensor(10);
 
 // generate constants
-float unitsDivisor = 1000.0; 
 double a1 = 129; //mm
 double a2 = 152; //mm
 double a3 = 152; //mm
@@ -26,10 +28,10 @@ double a4 = 129; //mm
 double a5 = 82; //mm
 
 // Pin declares
-int pwmPinR = 5; // PWM output pin for motor 
-int dirPinR = 8; // direction output pin for motor 
-int pwmPinL = 6; // PWM output pin for motor 
-int dirPinL = 7; // direction output pin for motor 
+int pwmPinR = 5; // PWM output pin for motor
+int dirPinR = 8; // direction output pin for motor
+int pwmPinL = 6; // PWM output pin for motor
+int dirPinL = 7; // direction output pin for motor
 int sensorPosPin = A4; // input pin for MR sensor
 int fsrPin = A3; // input pin for FSR sensor
 
@@ -65,12 +67,14 @@ boolean flippedHE = false;
 double OFFSETHE = 16300;
 double OFFSET_NEGHE = 15;
 
-// Kinematics 
-//variables 
+// Kinematics
+//variables
 double xh = 0;
 double yh = 0;
 double dxh = 0;
+double dxh_prev = 0;
 double dyh = 0;
+double dyh_prev = 0;
 
 //theta 1
 double theta1 = 0;
@@ -88,14 +92,16 @@ double dtheta5_prev;
 double dtheta5_prev2;
 
 // Force output variables
-double force = 0;           // force at the handle
-double forcey = 0;
+double forceX = 0;           // force at the handle
+double forceY = 0;
 
-double Tp = 0;              // torque of the motor pulley
-double TpL = 0;              // torque of the motor pulley
+double TR = 0;              // torque of the motor pulley
+double TL = 0;              // torque of the motor pulley
 
-double duty = 0;            // duty cylce (between 0 and 255)
-unsigned int output = 0;    // output command to the motor
+double dutyR = 0;            // duty cylce (between 0 and 255)
+unsigned int outputR = 0;    // output command to the motor
+double dutyL = 0;            // duty cylce (between 0 and 255)
+unsigned int outputL = 0;    // output command to the motor
 
 //workspace setup
 const float lengthWorkspace = 75.0 / unitsDivisor; //usable work length
@@ -136,43 +142,43 @@ unsigned long mSStart = 0;
 // --------------------------------------------------------------
 void setup() 
 {
+  angleSensor.init();
 
-  angleSensor.init(); 
-  
   // Set up serial communication
-  Serial.begin(9600);
-  
-  // Set PWM frequency 
-  setPwmFrequency(pwmPinR,1); 
-  
+  Serial.begin(115200);
+
+  // Set PWM frequency
+  setPwmFrequency(pwmPinR, 1);
+  setPwmFrequency(pwmPinL, 1);
+
   // Input pins
   pinMode(sensorPosPin, INPUT); // set MR sensor pin to be an input
   pinMode(fsrPin, INPUT);       // set FSR sensor pin to be an input
 
   // Output pins
-  pinMode(pwmPinR, OUTPUT);  // PWM pin for motor 
-  pinMode(dirPinR, OUTPUT);  // dir pin for motor 
+  pinMode(pwmPinR, OUTPUT);  // PWM pin for motor
+  pinMode(dirPinR, OUTPUT);  // dir pin for motor
 
-    // Output pins
-  pinMode(pwmPinL, OUTPUT);  // PWM pin for motor 
-  pinMode(dirPinL, OUTPUT);  // dir pin for motor 
-  
-  // Initialize motor 
+  // Output pins
+  pinMode(pwmPinL, OUTPUT);  // PWM pin for motor
+  pinMode(dirPinL, OUTPUT);  // dir pin for motor
+
+  // Initialize motor
   analogWrite(pwmPinR, 0);     // set to not be spinning (0/255)
   digitalWrite(dirPinR, LOW);  // set direction
 
-  // Initialize motor 
+  // Initialize motor
   analogWrite(pwmPinL, 0);     // set to not be spinning (0/255)
   digitalWrite(dirPinL, LOW);  // set direction
-  
+
   // Initialize position valiables
   lastLastRawPos = analogRead(sensorPosPin);
   lastRawPos = analogRead(sensorPosPin);
   flipNumber = 0;
 
-  // Initialize position variables 
-  angleSensor.getRawRotation(); // take reading in case first reading is spurious 
-  lastLastRawPosHE = angleSensor.getRawRotation(); //position from Hall Effect sensor 
+  // Initialize position variables
+  angleSensor.getRawRotation(); // take reading in case first reading is spurious
+  lastLastRawPosHE = angleSensor.getRawRotation(); //position from Hall Effect sensor
   lastRawPosHE = angleSensor.getRawRotation(); //position from Hall Effect sensor
 
   //setup virtual enviroment
@@ -184,15 +190,17 @@ void setup()
   
 }// end of setup loop
 
+// --------------------------------------------------------------
+// Main Loop
+// --------------------------------------------------------------
+
 //for counting loops
 int loopNumber = 0;
 
 //for not printing every time
 int doNotPrintEveryTime = 0;
-// --------------------------------------------------------------
-// Main Loop
-// --------------------------------------------------------------
 
+//for calulating loop time
 unsigned long mainLoopStart = millis();
 
 void loop()
@@ -265,23 +273,18 @@ void loop()
   //*** Section 2. Compute position in meters *******************
   //*************************************************************
 
-  // ADD YOUR CODE HERE
+    // ADD YOUR CODE HERE
   // Define kinematic parameters you may need
-     //double rh = ?;   //[m]
+  //double rh = ?;   //[m]
   // Step B.1: print updatedPos via serial monitor
-// if( doNotPrintEveryTime % (10) == 0 ){
-//  Serial.println(rawPosHE);
-//  //Serial.println(updatedPos);
-// }
+  //Serial.println((float)updatedPosHE,5);
   //Serial.println((float)updatedPos,5);
   // Step B.6: double ts = ?; // Compute the angle of the sector pulley (ts) in degrees based on updatedPos
-  double theta5 = (PI/180)*(updatedPos*-0.0129+155.5);
-  double theta1 = (PI/180)*(updatedPosHE*0.001470+38.09);
-//  Serial.print(theta5,5);
-//  Serial.print(" ");
-////  Serial.print("\t");
-//  Serial.print(theta1,5);
-//  Serial.println(" ");
+  double theta5 = (PI / 180) * (updatedPos * -0.0129 + 149.5);
+  double theta1 = (PI / 180) * (updatedPosHE * 0.001470 + 16.09);
+  //  Serial.print((float)theta5,5);
+  //  Serial.print("\t");
+  //  Serial.print((float)theta1,5);
   // Step B.7: xh = ?;       // Compute the position of the handle (in meters) based on ts (in radians)
   // Step B.8: print xh via serial monitor
 
@@ -301,34 +304,33 @@ void loop()
   theta5_prev2 = theta5_prev;
   theta5_prev = theta5;
 
-//  Serial.print("\t");
-//  Serial.print((float)dtheta5,5);
-//  Serial.print("\t");
-//  Serial.println((float)dtheta1,5);
-  
+  //  Serial.print("\t");
+  //  Serial.print((float)dtheta5,5);
+  //  Serial.print("\t");
+  //  Serial.println((float)dtheta1,5);
+
   //calculate positions 2 and 4
-  double P2x = a1*cos(theta1);
-  double P2y = a1*sin(theta1);
-  double P4x = a4*cos(theta5) - a5;
-  double P4y = a4*sin(theta5);
-  
+  double P2x = a1 * cos(theta1);
+  double P2y = a1 * sin(theta1);
+  double P4x = a4 * cos(theta5) - a5;
+  double P4y = a4 * sin(theta5);
+
+
+
   // calculate norms
-  double P42_norm = sqrt((P2x-P4x)*(P2x-P4x)+(P2y-P4y)*(P2y-P4y));
-  double P2h_norm = (a2*a2 - a3*a3 + P42_norm*P42_norm)/(2*P42_norm);
-  double P3h_norm = sqrt(a2*a2 - P2h_norm*P2h_norm);
+  double P42_norm = sqrt((P2x - P4x) * (P2x - P4x) + (P2y - P4y) * (P2y - P4y));
+  double P2h_norm = (a2 * a2 - a3 * a3 + P42_norm * P42_norm) / (2 * P42_norm);
+  double P3h_norm = sqrt(a2 * a2 - P2h_norm * P2h_norm);
 
   // calculate position h
-  double Phx = P2x + (P2h_norm/P42_norm)*(P4x-P2x);
-  double Phy = P2y + (P2h_norm/P42_norm)*(P4y-P2y);
+  double Phx = P2x + (P2h_norm / P42_norm) * (P4x - P2x);
+  double Phy = P2y + (P2h_norm / P42_norm) * (P4y - P2y);
   // calculate position 3
-  double P3x = Phx + (P3h_norm/P42_norm)*(P4y-P2y);
-  double P3y = Phy - (P3h_norm/P42_norm)*(P4x-P2x);
+  double P3x = Phx + (P3h_norm / P42_norm) * (P4y - P2y);
+  double P3y = Phy - (P3h_norm / P42_norm) * (P4x - P2x);
 
   xh = P3x;
   yh = P3y;
-  //Serial.print((float)xh,3);
-  //Serial.print(" : ");
-  //Serial.println((float)yh,3);
 
   //rename define for convenience
   double d = P42_norm;
@@ -336,36 +338,54 @@ void loop()
   double h = P3h_norm;
   // calculate partial derivatives
   // d1 partial derivatives
-  double d1x2 = -a1*sin(theta1);
-  double d1y2 = a1*cos(theta1);
+  double d1x2 = -a1 * sin(theta1);
+  double d1y2 = a1 * cos(theta1);
   double d1y4 = 0;
   double d1x4 = 0;
-  double d1d = ( (P4x-P2x)*(d1x4-d1x2) + (P4y-P2y)*(d1y4-d1y2 ))/d;
-  double d1b = d1d - ( d1d*(a2*a2 - a3*a3 + d*d))/(2*d*d);
-  double d1h = -b*d1b/h;
-  double d1yh = d1y2 + ( (d1b*d - d1d*b)/(d*d) )*(P4y-P2y)+(b/d)*(d1y4-d1y2);
-  double d1xh = d1x2+( (d1b*d-d1b*d)/(d*d) )*(P4x-P2x)+(b/d)*(d1x4-d1x2);
-  double d1x3 = d1xh + (h/d)*(d1y4 - d1y2) + (d1h*d - d1d*h)/(d*d)*(P4y - P2y);
-  double d1y3 = d1yh + (h/d)*(d1x4 - d1x2) + (d1h*d - d1d*h)/(d*d)*(P4x - P2x);
+  double d1d = ( (P4x - P2x) * (d1x4 - d1x2) + (P4y - P2y) * (d1y4 - d1y2 ) ) / d;
+  double d1b = d1d - ( d1d * (a2 * a2 - a3 * a3 + d * d)) / (2 * d * d);
+  double d1h = -b * d1b / h;
+  double d1yh = d1y2 + ( (d1b * d - d1d * b) / (d * d) ) * (P4y - P2y) + (b / d) * (d1y4 - d1y2);
+  double d1xh = d1x2 + ( (d1b * d - d1d * b) / (d * d) ) * (P4x - P2x) + (b / d) * (d1x4 - d1x2);
+  double d1y3 = d1yh - (h / d) * (d1x4 - d1x2) - (d1h * d - d1d * h) / (d * d) * (P4x - P2x);
+  double d1x3 = d1xh + (h / d) * (d1y4 - d1y2) + (d1h * d - d1d * h) / (d * d) * (P4y - P2y);
   // d5 partial derivatives
-  double d5x4 = -a4*sin(theta5);
-  double d5y4 = a4*cos(theta5);
+  double d5x4 = -a4 * sin(theta5);
+  double d5y4 = a4 * cos(theta5);
   double d5y2 = 0;
   double d5x2 = 0;
-  double d5d = ( (P4x-P2x)*(d5x4-d5x2) + (P4y-P2y)*(d5y4-d5y2) )/d;
-  double d5b = d5d - ( d5d*(a2*a2 - a3*a3 + d*d))/(2*d*d);
-  double d5h = -b*d1b/h;
-  double d5yh = d5y2 + ( (d5b*d - d5d*b)/(d*d) )*(P4y-P2y)+(b/d)*(d5y4-d5y2);
-  double d5xh = d5x2+( (d5b*d-d5b*d)/(d*d) )*(P4x-P2x)+(b/d)*(d5x4-d5x2);;
-  double d5x3 = d5xh + (h/d)*(d5y4 - d5y2) + (d5h*d - d5d*h)/(d*d)*(P4y - P2y);
-  double d5y3 = d5yh + (h/d)*(d5x4 - d5x2) + (d5h*d - d5d*h)/(d*d)*(P4x - P2x);
+  double d5d = ( (P4x - P2x) * (d5x4 - d5x2) + (P4y - P2y) * (d5y4 - d5y2) ) / d;
+  double d5b = d5d - ( d5d * (a2 * a2 - a3 * a3 + d * d)) / (2 * d * d);
+  double d5h = -b * d5b / h;
+  double d5yh = d5y2 + ( (d5b * d - d5d * b) / (d * d) ) * (P4y - P2y) + (b / d) * (d5y4 - d5y2);
+  double d5xh = d5x2 + ( (d5b * d - d5d * b) / (d * d) ) * (P4x - P2x) + (b / d) * (d5x4 - d5x2);
+  double d5y3 = d5yh - (h / d) * (d5x4 - d5x2) - (d5h * d - d5d * h) / (d * d) * (P4x - P2x);
+  double d5x3 = d5xh + (h / d) * (d5y4 - d5y2) + (d5h * d - d5d * h) / (d * d) * (P4y - P2y);
   //calculate velocity from omega (dTheta{i})
-  double dxh = d1x3*dtheta1 + d5x3*dtheta5;
-  double dyh = d1y3*dtheta1 + d5x3*dtheta5; 
-  //calculate torque from force 
-  //Serial.print((float)dxh,3);
-  //Serial.print(" : ");
-  //Serial.println((float)dyh,3);
+  double dxh = (d1x3 * dtheta1 + d5x3 * dtheta5)*0.1+dxh_prev*0.9;
+ 
+  double dyh = (d1y3 * dtheta1 + d5y3 * dtheta5)*0.1+dyh_prev*0.9;
+
+  if (isnan(dxh)){
+    dxh = 0;
+  }
+  
+  if (isnan(dyh)){
+    dyh = 0;
+  }
+  
+  dxh_prev = dxh;
+  dyh_prev = dyh;
+  
+  //calculate torque from force
+  static int  myCount = 0;
+  if (myCount == 100){
+
+    myCount = 0;
+  }
+  else {
+    myCount++;
+  }
 
   //*************************************************************
   //*** Section 3.Rendering Algorithms: ************************* 
@@ -373,7 +393,7 @@ void loop()
   //*************************************************************
 
 //for now, wait until in position before starting rendering
-if ( (millis()- mainLoopStart)/64 >= 1000*0){
+if ( (millis()- mainLoopStart)/64 >= 1000*5){
   ENABLE_MASS_SPRING_DAMP = true;
 }
 
@@ -427,9 +447,7 @@ if (ENABLE_MASS_SPRING_DAMP == true){
   
     clayIndexClosest = indexMin(xdiffUserMass); //choose the clay element to interact with as the element with minimum xdiffUserMass 
   
-    int clayIndexNext = points + 1; //should start as impossible index
-
-    
+    int clayIndexNext = points + 1; //should start as impossible index    
   
     //find 2nd element depending on if  xUser  > or < xMass
     if (xUser > xmass[clayIndexClosest]){ // inequality could be if xdiffUserMass[clayIndexClosest] <= 0 
@@ -477,16 +495,14 @@ if (ENABLE_MASS_SPRING_DAMP == true){
         
         float d = abs ( xLineWeight * xUser + yLineWeight * yUser  + lineConstant ) / sqrt(xLineWeight*xLineWeight + yLineWeight*yLineWeight); //penetration distance
 
-        Serial.print("d");
-        Serial.println(d);
-        ardprintf("d xLineWeight yLineWeight lineConstant %f %d %d %d", d, xLineWeight, yLineWeight, lineConstant);
+        //ardprintf("d xLineWeight yLineWeight lineConstant %f %d %d %d", d, xLineWeight, yLineWeight, lineConstant);
         
         userForceMag = kUser * d; 
         
         userForce[clayIndexClosest] = -userForceMag * abs( ( xUser - xmass[clayIndexNext] ) / lengthBetween ); //need to add negative such that clay is being pushed inwards
         userForce[clayIndexNext] = -userForceMag * abs( ( xUser - xmass[clayIndexClosest] ) / lengthBetween ); //see above
 
-        ardprintf("d userForceMag userForce[clayIndexClosest] userForce[clayIndexNext] %f %f %f %f", d, userForceMag, userForce[clayIndexClosest], userForce[clayIndexNext]);
+        //ardprintf("d userForceMag userForce[clayIndexClosest] userForce[clayIndexNext] %f %f %f %f", d, userForceMag, userForce[clayIndexClosest], userForce[clayIndexNext]);
               
     }  
   
@@ -498,8 +514,8 @@ if (ENABLE_MASS_SPRING_DAMP == true){
   
     //force at handle, if no contact found userForceMag will be 0 
     //force applied to the User should be perpendicular to the line
-    double forceX = - userForceMag * unitDirectionY; //need multiply by negative unitDirectionY weight to have the user direction point OUT of the clay
-    double forceY = userForceMag * unitDirectionX;
+    //double forceX = - userForceMag * unitDirectionY; //need multiply by negative unitDirectionY weight to have the user direction point OUT of the clay
+    //double forceY = userForceMag * unitDirectionX;
    
    ///****Calculate total force on clay****///
    
@@ -511,7 +527,7 @@ if (ENABLE_MASS_SPRING_DAMP == true){
   
    ///****Calculate resulting motion****///
   
-   //update all the masses velocities, accelerations, and forces
+      //update all the masses velocities, accelerations, and forces
       //calculate acceleration from F = ma
       UpdateClayAccelerations(clayTotalForce, accMass);   
   
@@ -558,12 +574,16 @@ if (ENABLE_MASS_SPRING_DAMP == true){
   //    Serial.print(yUser,6);
   //    Serial.println(); 
   
-      SendArrayOverSerial(xmass);
-      SendArrayOverSerial(ymass);
-      Serial.print(1000.0 * xUser,0);
-      Serial.print(",");
-      Serial.print(1000.0 * yUser,0);
-      Serial.println(); 
+//      SendArrayOverSerial(xmass);
+//      SendArrayOverSerial(ymass);
+//      Serial.print(1000.0 * xUser,0);
+//      Serial.print(",");
+//      Serial.print(1000.0 * yUser,0);
+//      Serial.print(",");
+//      Serial.print(forceX,0);
+//      Serial.print(",");
+//      Serial.print(forceY,0);
+//      Serial.println(); 
   //    Serial.print(" "); 
   //    Serial.print(xh,5);
   //    Serial.print(",");
@@ -577,30 +597,86 @@ if (ENABLE_MASS_SPRING_DAMP == true){
   
   // Step C.1: force = ?; // You can  generate a force by assigning this to a constant number (in Newtons) or use a haptic rendering / virtual environment
   // Step C.2: Tp = ?;    // Compute the require motor pulley torque (Tp) to generate that force using kinematics
+
+  // ADD YOUR CODE HERE
+  // Define kinematic parameters you may need
+  //VIRTUAL OBJECT HERE
+  // calculate forceX
+  // calculate forceY
+  forceX = 0.0;
+  forceY = 0.0;
+  // FORCE SIMULATION HERE
+//  TL = -1;
+//  TR = -1;
+  TL = d1x3*forceX + d1y3*forceY;
+  TR = d5x3*forceX + d5y3*forceY;
+
+  if (isnan(TL)){
+    TL = 0;
+  }
+  
+  if (isnan(TR)){
+    TR = 0;
+  }
+  
+//  Serial.print(TL,5);
+//  Serial.print(" : ");
+//  Serial.println(TR,5);
+  
+  // Step C.1: force = ?; // You can  generate a force by assigning this to a constant number (in Newtons) or use a haptic rendering / virtual environment
+  // Step C.2: Tp = ?;    // Compute the require motor pulley torque (Tp) to generate that force using kinematics
+//  Serial.print(TR);
+//  Serial.print("\t");
+//  Serial.println(TL);
  
   //*************************************************************
   //*** Section 4. Force output (do not change) *****************
   //*************************************************************
   
-  // Determine correct direction for motor torque
-  if(force > 0) { 
+//COMMENT THIS TO CHECK TORQUE VALUE AND SIGN
+  // Determine correct direction for RIGHT motor torque
+  if (TR > 0) {
     digitalWrite(dirPinR, HIGH);
   } else {
     digitalWrite(dirPinR, LOW);
   }
 
   // Compute the duty cycle required to generate Tp (torque at the motor pulley)
-  duty = sqrt(abs(Tp)/0.0183);
+  dutyR = sqrt(abs(TR) / 0.0183);
 
   // Make sure the duty cycle is between 0 and 100%
-  if (duty > 1) {            
-    duty = 1;
-  } else if (duty < 0) { 
-    duty = 0;
-  }  
-  output = (int)(duty* 255);   // convert duty cycle to output signal
-  analogWrite(pwmPinR,output);  // output the signal
+  if (dutyR > 1) {
+    dutyR = 1;
+  } else if (dutyR < 0) {
+    dutyR = 0;
+  }
+  outputR = (int)(dutyR * 255);  // convert duty cycle to output signal
+  analogWrite(pwmPinR, outputR); // output the signal
+
+
+  // Determine correct direction for LEFT  motor torque
+  if (TL > 0) {
+    digitalWrite(dirPinL, HIGH);
+  } else {
+    digitalWrite(dirPinL, LOW);
+  }
+
+  // Compute the duty cycle required to generate Tp (torque at the motor pulley)
+  dutyL = sqrt(abs(TL) / 0.0183);
+
+  // Make sure the duty cycle is between 0 and 100%
+  if (dutyL > 1) {
+    dutyL = 1;
+  } else if (dutyL < 0) {
+    dutyL = 0;
+  }
+  outputL = (int)(dutyL * 255);  // convert duty cycle to output signal
+  analogWrite(pwmPinL, outputL); // output the signal
+  //STOP COMMENT OUT HERE
 }
+
+
+
 
 // --------------------------------------------------------------
 // Function to set PWM Freq -- DO NOT EDIT
